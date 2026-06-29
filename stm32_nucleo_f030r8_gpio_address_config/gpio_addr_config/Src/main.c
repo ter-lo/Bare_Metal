262797b2d14b4f3cfcb8bdd1d5b61a096581d0ca
ter-lo <260774354+ter-lo@users.noreply.github.com>
2026-06-29 08:59:22 -0700

    Added a toggle loop. GPIOA_ODR ^= USER_LED2; for(int i = 0; i < 1000000; i++){}

--- stm32_nucleo_f030r8_gpio_address_config/gpio_addr_config/Src/main.c
+++ stm32_nucleo_f030r8_gpio_address_config/gpio_addr_config/Src/main.c
@@ -18,23 +18,27 @@
 
 #define PERIPH_BASE			0x40000000UL
 
-/*
- * AHB1 to access RCC
- */
+/* AHB1 to access RCC */
 #define AHB1_OFFSET			0x00020000UL
-#define AHB1_PERIPH_BASE	PERIPH_BASE + AHB1_OFFSET
+#define AHB1_PERIPH_BASE	PERIPH_BASE + AHB1_OFFSET		// 0x4002 0000
+
+/* AHB2 to access GPIOA */
+#define AHB2_OFFSET			0x08000000UL
+#define AHB2_PERIPH_BASE	PERIPH_BASE + AHB2_OFFSET		// 0x4800 0000
+#define GPIOA_BASE			AHB2_PERIPH_BASE				// 0x4800 0000
 
 /*
- * AHB2 to access GPIOA
+ * Issue: Incorrectly set RCC_OFFSET to 0x00021000UL
+ *
+ * Solution:  Because AHB1_PERIPH_BASE is equal to 0x40020000
+ * 			  When adding to RCC_BASE while defining RCC_AHBENR
+ * 			  originally equalled 40041000 (notice the rogue 4 on
+ * 			  bit 4. I changed RCC_OFFSET to 0x1000UL allowing the
+ * 			  2 on bit 4 to stay.
  */
-#define AHB2_OFFSET			0x08000000UL
-#define AHB2_PERIPH_BASE	PERIPH_BASE + AHB2_OFFSET
-#define GPIOA_OFFSET		0x08000000UL
-#define GPIOA_BASE			AHB2_PERIPH_BASE + GPIOA_OFFSET
-
-#define RCC_OFFSET			0x00021000UL
-#define RCC_BASE			AHB1_PERIPH_BASE + RCC_OFFSET
-#define RCC_AHBENR_OFFSET	0x14
+#define RCC_OFFSET			0x1000UL
+#define RCC_BASE			AHB1_PERIPH_BASE + RCC_OFFSET	// 0x4002 0000 + 0000 1000 = 0x4002 1000
+#define RCC_AHBENR_OFFSET	0x14							// 0x0000 0014
 /*
  * Typecast and dereference to a pointer
  * This creates the register
@@ -58,28 +62,30 @@
 
 #define USER_LED2			(1U<<5)
 
-/*
- * Write the execution
- */
+/* Write the execution */
 
 int main(void)
 {
-	/*
-	 * Enable clock access to port A
-	 */
+	/* Enable clock access to port A */
 	RCC_AHBENR |= GPIOA_CLK_EN;
 
-	/*
-	 * Configure LED Pin as output pins
-	 */
+	/* Configure LED Pin as output pins */
 	GPIOA_MODER |= USER_LED2_MODER;
 
-	/*
-	 * Turn on LED using a while loop
-	 */
+	/* Turn on LED using a while loop */
 	while(1)
 	{
-		GPIOA_ODR |= USER_LED2;
+		/* Turn on all LED (LD2) */
+		// GPIOA_ODR |= USER_LED2;
+
+		/* Toggle LED (LD2) using XOR ^
+		 * I'm targeting bit 5 therefore
+		 * XOR flips only bit 5 to produce
+		 * the toggle effect
+		 */
+		GPIOA_ODR ^= USER_LED2;
+		for(int i = 0; i < 1000000; i++){}
+
 	}
 
 
